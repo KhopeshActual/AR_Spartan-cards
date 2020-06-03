743598b7a3d73d9bae1217d7783c2c92b3e9e24f
Jon <61843462+KhopeshActual@users.noreply.github.com>
2020-06-03 12:27:17 -0700

    UI Update push

---
+++ Assets/MYScripts/change_active_character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class change_active_character : MonoBehaviour
+{
+    public GameObject Spartan;
+    public GameObject Guardian;
+
+    bool isSpartan = true;
+
+    public void Swap()
+    {
+        isSpartan = !isSpartan;
+
+        Spartan.SetActive(isSpartan);
+
+        Guardian.SetActive(!isSpartan);
+
+    }
+}
